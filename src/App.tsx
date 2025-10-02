1de36c5a8bd9d12c160983f56684fa48e020ae38
Laxman-Singh-Bisht <laxmanbisht902@gmail.com>
2025-10-02 15:44:51 +0530

    Using UseState hook to show increase and decrease value of counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 //import Users from "./Component/Users"
 //import './App.css'
+import { useState } from "react"
+
 function App() {
-  let counter = 15
+  const [counter, setCounter] = useState(15)
+
+  // let counter = 15
 
   const addValue = () =>{  // using arrow function we can use normal function as well.
-    console.log("clicked",counter)
-    counter = counter + 1
-  }
-  return (
+    if(counter <= 19){   // Used to that Add Value never be more than 20   
+      setCounter(counter+1)
+    }
     
+  }
+
+  const removeValue = () =>{
+    if(counter >=1 ){    // Used to that Remove Value never be less than 0
+      setCounter(counter-1)
+    }
+  }
+
+  return ( 
   <>
     <h1>Learn React </h1>
     <h2>Counter Value :{counter}</h2>
-    <button onClick={addValue}>Add Value</button>
-    <button >decrease Value</button>
+    <button onClick={addValue}>Add Value{counter}</button>
+    <br/>
+    <button onClick={removeValue} >Remove Value {counter}</button>
+    <p>footer: {counter}</p>
   </>
     
   )

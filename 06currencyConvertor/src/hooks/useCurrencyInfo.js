bfc2803512552eaf39e992d0589d9c1bd5338194
Laxman-Singh-Bisht <laxmanbisht902@gmail.com>
2025-10-13 23:52:23 +0530

    create currency convertor app

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
+function useCurrencyInfo(currency){    
     const[data, setData] = useState({})
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.
+    useEffect(()=>{       
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.
             json`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
             console.log(data);
-    }, [currency])
+    }, [currency, setData, data])
     console.log(data);
     return data
 }
